39c1c5387acdb3ab3dc4a342b300c4aa514b82f9
MIDEZA-22 <mijail1993.mdzl@gmail.com>
2023-02-09 00:01:05 -0500

    Modificación de la estructura original del Proyecto

--- src/config.py
+++
@@ -1,4 +0,0 @@
-import os
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/models/pockedex.py
+++
@@ -1,7 +0,0 @@
-from src.db import db
-from sqlalchemy.sql import func
-
-
-class pokedex(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100))
