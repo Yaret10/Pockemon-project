efd2e3c3cca4b41a34eed3dadb898179b03e889a
MIDEZA-22 <mijail1993.mdzl@gmail.com>
2023-02-09 19:19:19 -0500

    Implementación de la ruta principal 'principal_router.py' y eliminación de código erroneo en el archivo '__init__.py' que esta dentro de la carpeta 'app'

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,1 @@
-from flask import Flask
-from app.routes.r_pockemon import pockemon_router
-from flask_sqlalchemy import SQLAlchemy
-from .config import Config
-from flask_migrate import Migrate
 
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
-    app.register_blueprint(pockemon_router)
-    return app
